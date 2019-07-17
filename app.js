af9600838f4a2fe48cb8c776dedb8d5c9575777d
BURCHILL <jarodburchill@gmail.com>
2019-07-17 15:25:04 -0400

    Animation Change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const headline = document.querySelector('.headline');
 const tl = new TimelineMax();
 
 tl.fromTo(hero, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
-  .fromTo(hero, 1, { width: "100%" }, { width: "80%", ease: Power2.easeInOut })
+  .fromTo(hero, 1, { width: "60%" }, { width: "100%", ease: Power2.easeInOut })
   .fromTo(slider, 1.5, { x: "-100%" }, { x: '0%', ease: Power2.easeInOut }, "-=1")
   .fromTo(logo, 1, { opacity: 0, x: 100 }, { opacity: 1, x: 0 }, "-=0.4")
   .fromTo(icon, 1, { opacity: 0, x: -100 }, { opacity: 1, x: 0 }, "-=1")
