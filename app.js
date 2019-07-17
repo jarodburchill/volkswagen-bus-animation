3aaaea9d3ec231c829a6f394f1cdd54c51f0a91f
BURCHILL <jarodburchill@gmail.com>
2019-07-17 15:31:06 -0400

    New speed

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const headline = document.querySelector('.headline');
 const tl = new TimelineMax();
 
 tl.fromTo(hero, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
-  .fromTo(hero, 1, { width: "100%" }, { width: "80%", ease: Power2.easeInOut })
-  .fromTo(slider, 1.5, { x: "-100%" }, { x: '0%', ease: Power2.easeInOut }, "-=1")
+  .fromTo(hero, 1.5, { width: "100%" }, { width: "80%", ease: Power2.easeInOut })
+  .fromTo(slider, 1.5, { x: "-100%" }, { x: '0%', ease: Power2.easeInOut }, "-=1.5")
   .fromTo(logo, 1, { opacity: 0, x: 100 }, { opacity: 1, x: 0 }, "-=0.4")
   .fromTo(icon, 1, { opacity: 0, x: -100 }, { opacity: 1, x: 0 }, "-=1")
   .fromTo(headline, 1, { opacity: 0, x: -100 }, { opacity: 1, x: -0 }, "-=1")
